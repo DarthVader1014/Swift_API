23f0d9aea8514ae9fb551607c0ee8c1151602468
DarthVader1014 <84717093+DarthVader1014@users.noreply.github.com>
2022-05-03 16:14:54 +0200

    Add files via upload
    Checked

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Swift_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+        //constructor
+        public ProductController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+
+        //Retrieve product info
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Get()
+        {
+            return Ok(await _dataContext.Products.ToListAsync());
+        }
+
+        [HttpGet("{ProductId}")]
+        public async Task<ActionResult<List<Product>>> Get(int ProductId)
+        {
+            var product = await _dataContext.Products.FindAsync(ProductId);
+            if (product == null)
+                return BadRequest("Product not found.");
+            return Ok(product);
+        }
+
+        //Add new product
+        [HttpPost]
+        public async Task<ActionResult<List<Product>>> AddProduct(Product product)
+        {
+            _dataContext.Products.Add(product);
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(await _dataContext.Products.ToListAsync());
+        }
+
+        //Update product records
+        [HttpPut]
+        public async Task<ActionResult<List<Product>>> UpdateProduct(Product request)
+        {
+            var dbproduct = await _dataContext.Products.FindAsync(request.ProductId);
+            if (dbproduct == null)
+                return BadRequest("Product not found.");
+
+            dbproduct.ProductId = request.ProductId;
+            dbproduct.ProductName = request.ProductName;
+            dbproduct.ProductPrice = request.ProductPrice;
+            dbproduct.CategoryId = request.CategoryId;
+            dbproduct.ProductDesc = request.ProductDesc;
+
+
+            await _dataContext.SaveChangesAsync();
+
+
+            return Ok(await _dataContext.Products.ToListAsync());
+
+        }
+
+        //Delete product records
+        [HttpDelete("{ProductId}")]
+
+        public async Task<ActionResult<List<Product>>> Delete(int ProductId)
+        {
+            var dbproduct = await _dataContext.Products.FindAsync(ProductId);
+            if (dbproduct == null)
+                return BadRequest("Product not found.");
+
+            _dataContext.Products.Remove(dbproduct);
+            await _dataContext.SaveChangesAsync();
+
+            return Ok(await _dataContext.Products.ToListAsync());
+        }
+
+    }
+}
---
+++ Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Swift_API
+{
+    public class Order
+    {
+        [Key]
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int CustomerId { get; set; }
+        public int Quantity { get; set; }   
+
+    }
+}
